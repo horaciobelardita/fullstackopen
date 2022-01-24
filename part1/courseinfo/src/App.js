ee94f79c8cc68bca53d57a1a426fded568520f1f
Horacio Belardita <horaciobelardita@gmail.com>
2022-01-24 18:10:47 -0300

    add prettier config

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,20 +1,20 @@
-import "./App.css";
+import './App.css'
 
-const Header = ({ course }) => <h1>{course}</h1>;
+const Header = ({ course }) => <h1>{course}</h1>
 
 const Part = ({ name, exercises }) => (
   <p>
     {name} {exercises}
   </p>
-);
+)
 
 const Total = ({ parts }) => {
   const totalExercises = parts.reduce(
     (total, part) => total + part.exercises,
     0
-  );
-  return <p>Total: {totalExercises}</p>;
-};
+  )
+  return <p>Total: {totalExercises}</p>
+}
 
 const Content = ({ parts }) => {
   return (
@@ -23,36 +23,36 @@ const Content = ({ parts }) => {
         <Part key={part.name} {...part} />
       ))}
     </div>
-  );
-};
+  )
+}
 
 const App = () => {
   const course = {
-    name: "Half Stack application development",
+    name: 'Half Stack application development',
 
     parts: [
       {
-        name: "Fundamentals of React",
+        name: 'Fundamentals of React',
         exercises: 10,
       },
       {
-        name: "Using props to pass data",
+        name: 'Using props to pass data',
         exercises: 7,
       },
       {
-        name: "State of a component",
+        name: 'State of a component',
         exercises: 14,
       },
     ],
-  };
-  const { parts, name } = course;
+  }
+  const { parts, name } = course
   return (
     <div className="App">
       <Header course={name} />
       <Content parts={parts} />
       <Total parts={parts} />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
